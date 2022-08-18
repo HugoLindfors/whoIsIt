3f51a88368abdfcd90c6b9b7408cb644e73c3853
Markus Piotrowski <Markuspiotrowski@live.com>
2022-08-18 10:23:19 +0200

    JsChange

--- js/js.js
+++ js/js.js
@@ -1,74 +1,84 @@
 const people = [
-    {
-        name: "Albin Norbäck",
-        imgsrc: "/img/Albin.jpeg"
-    },
-    {
-        name: "Amir Ismail",
-        imgsrc: "/img/Amir.jpeg"
-    },
-    {
-        name: "Charlie Nylund",
-        imgsrc: "/img/Charlie.jpeg"
-    },
-    {
-        name: "Hugo Lindfors",
-        imgsrc: "/img/Hugo.jpeg"
-    },
-    {
-        name: "Markus Piotrowski",
-        imgsrc: "/img/Markus.jpeg"
-    },
-    {
-        name: "Mathias Åhlander",
-        imgsrc: "/img/Mathias.jpeg"
-    },
-    {
-        name: "Robin Barwari",
-        imgsrc: "/img/Robin.jpeg"
-    },
-    samuel = {
-        name: "Samuel Broman",
-        imgsrc: "/img/Samuel.jpeg"
-    }
-]
+  {
+    name: "Albin Norbäck",
+    imgsrc: "/img/Albin.jpeg",
+  },
+  {
+    name: "Amir Ismail",
+    imgsrc: "/img/Amir.jpeg",
+  },
+  {
+    name: "Charlie Nylund",
+    imgsrc: "/img/Charlie.jpeg",
+  },
+  {
+    name: "Hugo Lindfors",
+    imgsrc: "/img/Hugo.jpeg",
+  },
+  {
+    name: "Markus Piotrowski",
+    imgsrc: "/img/Markus.jpeg",
+  },
+  {
+    name: "Mathias Åhlander",
+    imgsrc: "/img/Mathias.jpeg",
+  },
+  {
+    name: "Robin Barwari",
+    imgsrc: "/img/Robin.jpeg",
+  },
+  (samuel = {
+    name: "Samuel Broman",
+    imgsrc: "/img/Samuel.jpeg",
+  }),
+];
 const button1 = document.querySelector(".b1");
 const button2 = document.querySelector(".b2");
 const button3 = document.querySelector(".b3");
 const button4 = document.querySelector(".b4");
 const imageOfPersonsFace = document.querySelector("#image-of-persons-face");
 
-const buttonText = [document.querySelector("#name-1"), document.querySelector("#name-2"),document.querySelector("#name-3"),document.querySelector("#name-4")]
+const buttonText = [
+  document.querySelector("#name-1"),
+  document.querySelector("#name-2"),
+  document.querySelector("#name-3"),
+  document.querySelector("#name-4"),
+];
 
 let correctAnswer = "1";
 
-button1.addEventListener('click', insertPeople);
+button1.addEventListener("click", insertPeople);
 
 function buttonPressed(input) {
-    if (input === correctAnswer) {
-
-        alert("Du väl rätt");
-    } else {
-        alert("Du hade fel!");
-    }
-};
-
-function insertPeople() {
-    console.log("Test");
-    let numbersArray=[];
-    buttonText.forEach(element => {
-        let tempRandom= Math.floor(Math.random() * people.length);
-        numbersArray.push(tempRandom)
-        element.innerHTML=people[tempRandom].name;
-    }
-    );
-    let randomNr = numbersArray[Math.floor(Math.random() * numbersArray.length)];
-
-    imageOfPersonsFace.src = people[randomNr].imgsrc;
+  if (input === correctAnswer) {
+    alert("Du väl rätt");
+  } else {
+    alert("Du hade fel!");
+  }
 }
 
+function insertPeople() {
+    let peopleCopy = [...people];
+    let randomPeopleArray = [];
 
+  for (let index = 0; index < peopleCopy.length; index++) {
+    let random = Math.floor(Math.random() * peopleCopy.length);
+    randomPeopleArray.push(peopleCopy[random]);
+    peopleCopy.splice(random, 1);
+  }
+  let peoplePopedArray = [];
+  console.log(randomPeopleArray);
+  buttonText.forEach((element) => {
+    let personPoped = randomPeopleArray.pop();
+    console.log(personPoped);
+    element.innerHTML = personPoped.name;
+    peoplePopedArray.push(personPoped);
+  });
+  let randomNr =
+    peoplePopedArray[Math.floor(Math.random() * peoplePopedArray.length)];
 
+  imageOfPersonsFace.src = peoplePopedArray[randomNr].imgsrc;
+}
 
 /*
 function nextPerson() {
